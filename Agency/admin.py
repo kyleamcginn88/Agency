da2b9bc3eb662d10f7ae9daa785f451f50d83ee2
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-02-17 16:43:07 -0700

    Added some html updates

--- Agency/admin.py
+++ Agency/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Contest1, Friend
+from models import Contest1, Friend, Contest2
 
 admin.site.register(Contest1)
+admin.site.register(Contest2)
 admin.site.register(Friend)
