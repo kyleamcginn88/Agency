5c05b18c164880247fa995454b4ba230b9399c11
kyleamcginn88 <kyle.a.mcginn@gmail.com>
2015-02-18 16:18:35 -0700

    Removed services page

--- Agency/views.py
+++ Agency/views.py
@@ -24,10 +24,6 @@ def about(request):
     return render(request, 'about.html', {})
 
 
-def services(request):
-    return render(request, 'services.html', {})
-
-
 def contact(request):
     return render(request, 'contact.html', {})
 
